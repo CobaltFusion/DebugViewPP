cca0e383ef531868bd343da82aecb08ab2135f39
Gert-Jan de Vos <gert-jan.de.vos@on.nl>
2014-01-01 19:10:39 +0100

    Automated build number updates in resource file.

--- DebugView++/UpdateBuildNr.cmd
+++ DebugView++/UpdateBuildNr.cmd
@@ -5,5 +5,8 @@ for /f "tokens=3,4,5,* delims=, " %%i in (%file%) do (
 	set minor=%%j
 	set inc=%%k
 	set /a buildnr=%%l+1
+	goto break
 )
+:break
 echo #define VERSION %major%,%minor%,%inc%,%buildnr% > %file%
+echo #define VERSION_STR "%major%.%minor%.%inc%.%buildnr%" >> %file%
