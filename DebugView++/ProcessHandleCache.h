e77a173e823924473df801e107129003c3833f58
janwilmans@gmail.com <janwilmans@gmail.com>
2014-01-21 23:53:35 +0100

    add class ProcessHandleCache

---
+++ DebugView++/ProcessHandleCache.h
@@ -0,0 +1,29 @@
+// (C) Copyright Gert-Jan de Vos and Jan Wilmans 2013.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// Repository at: https://github.com/djeedjay/DebugViewPP/
+
+#pragma once
+#include "Win32Lib.h"
+
+namespace fusion {
+namespace debugviewpp {
+
+typedef std::vector<DWORD> Pids;
+
+class ProcessHandleCache
+{
+public:
+	~ProcessHandleCache();
+
+	void Add(DWORD pid, HANDLE handle);
+	Pids Cleanup();
+
+private:
+	std::map<DWORD, Handle> m_cache;
+};
+
+} // namespace debugviewpp 
+} // namespace fusion
