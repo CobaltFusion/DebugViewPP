71be47072bbe56f922f5f4ef4a9336d6a5018fe7
janwilmans@gmail.com <janwilmans@gmail.com>
2014-01-24 01:28:46 +0100

    started implementing class CircularBuffer, far from done.

--- DebugView++/CircularBuffer.cpp
+++ DebugView++/CircularBuffer.cpp
@@ -7,8 +7,12 @@
 
 #pragma once
 
+#define _SCL_SECURE_NO_WARNINGS
+
 #include "stdafx.h"
 #include "CircularBuffer.h"
+#include "DBWinBuffer.h"
+#include "Win32Lib.h"
 
 namespace fusion {
 namespace debugviewpp {
@@ -20,14 +24,67 @@ CircularBuffer::CircularBuffer(size_t size) :
 	m_pWrite(m_pBegin)
 {
 }
+	
+bool CircularBuffer::Empty()
+{
+	return false;
+}
+
+bool CircularBuffer::Full()
+{
+	const long maxMessageSize = sizeof(double) + sizeof(FILETIME) + sizeof(HANDLE) + sizeof(DbWinBuffer);
+
+	//	return (m_pWrite >= (m_pEnd-maxMessageSize));	// ! wrong, must wrap at the end of the buffer
+
+	// todo: create either operators or methods to do CircularBuffer arithmetic
+	return false;
+}
+
+const char* CircularBuffer::ReadMessage()
+{
+	return 0;
+}
+
+void CircularBuffer::WriteMessage(const char* message)
+{
+	size_t len = strlen(message);
+	std::copy(message, message+len, stdext::checked_array_iterator<char*>(m_pWrite, m_pEnd-m_pWrite));
+	m_pWrite += len;
+}
 
 void CircularBuffer::Add(double time, FILETIME systemTime, HANDLE handle, const char* message)
 {
+	if (Full())
+		WaitForReader();
+	Write(time);
+	Write(systemTime);
+	Write(handle);
+	WriteMessage(message);
+}
 
+void CircularBuffer::WaitForReader()
+{
+    auto predicate = [this] { return !Full(); };
+
+	while (Full())
+	{
+		boost::mutex waitingLock;
+		boost::unique_lock<boost::mutex> lock(waitingLock);
+		m_triggerRead.timed_wait(lock, boost::posix_time::seconds(1), predicate);
+	}
 }
 
 Lines CircularBuffer::GetLines()
 {
+	Lines lines;
+	while (!Empty())
+	{
+		Handle handle(Read<HANDLE>());
+		DWORD pid = 0;			
+		std::string processName;
+		lines.push_back(Line(Read<double>(), Read<FILETIME>(), pid, processName, ReadMessage()));
+	}
+	m_triggerRead.notify_all();
 	return Lines();
 }
 
--- DebugView++/CircularBuffer.h
+++ DebugView++/CircularBuffer.h
@@ -20,13 +20,41 @@ public:
 
 	void Add(double time, FILETIME systemTime, HANDLE handle, const char* message);
 	Lines GetLines();
-
+	
 private:
+	bool Empty();
+	bool Full();
+	void WaitForReader();
+
+	template <class T> T Read();
+	const char* ReadMessage();
+
+	template <class T> void Write(T type);
+	void WriteMessage(const char* message);
+
 	char* m_pBegin;
 	char* m_pEnd;
 	const char* m_pRead;
 	char* m_pWrite;
+
+	boost::condition_variable m_triggerRead;
 };
+
+template <class T> 
+T CircularBuffer::Read()
+{
+	auto pBuffer = (T *) m_pRead;
+	m_pRead += sizeof(T);				// ! wrong, must wrap at the end of the buffer
+	return *pBuffer;
+}
+
+template <class T> 
+void CircularBuffer::Write(T value)
+{
+	auto pBuffer = (T *) m_pWrite;
+	*pBuffer = value;					// ! wrong, must wrap at the end of the buffer
+	m_pWrite += sizeof(T);
+}
 
 } // namespace debugviewpp 
 } // namespace fusion
