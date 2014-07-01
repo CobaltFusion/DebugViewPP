5126ab1016b9476bc2623f8a66ee2fa0002ad487
janwilmans@gmail.com <janwilmans@gmail.com>
2014-07-01 23:53:55 +0200

    experimental SocketReader reading from debugview in agent mode

--- DebugView++Lib/SocketReader.cpp
+++ DebugView++Lib/SocketReader.cpp
@@ -10,22 +10,81 @@
 #include "DebugView++Lib/SocketReader.h"
 #include "DebugView++Lib/LineBuffer.h"
 
+#include <boost/asio.hpp> 
+#include <boost/array.hpp>
+
 namespace fusion {
 namespace debugviewpp {
 
-SocketReader::SocketReader(Timer& timer, ILineBuffer& linebuffer, const std::wstring& pathName, const std::wstring& args) :
+SocketReader::SocketReader(Timer& timer, ILineBuffer& linebuffer) :
 	PassiveLogSource(timer, SourceType::Pipe, linebuffer, 40)
 {
 	SetDescription(L"socket");
+
+	m_thread = boost::thread(&SocketReader::Loop, this);
 }
 
 SocketReader::~SocketReader()
 {
 }
 
-bool SocketReader::AtEnd() const
+void SocketReader::Loop()
 {
-	return false;
+	using namespace boost::asio;
+	io_service io_service; 
+	ip::tcp::resolver resolver(io_service);
+	ip::tcp::socket socket(io_service); 
+
+	ip::tcp::resolver::query localAgent("127.0.0.1", "2020"); 
+	auto endpoint_iterator = resolver.resolve(localAgent);
+	ip::tcp::resolver::iterator end;
+	boost::system::error_code error = boost::asio::error::host_not_found;
+
+    while (error && endpoint_iterator != end)
+    {
+      socket.close();
+      socket.connect(*endpoint_iterator++, error);
+    }
+    if (error)
+      throw boost::system::system_error(error);
+
+	boost::array<unsigned char, 20> startBuf = { 
+		0x24, 0x00, 0x05, 0x83,
+		0x04, 0x00, 0x05, 0x83,
+		0x08, 0x00, 0x05, 0x83,
+		0x28, 0x00, 0x05, 0x83, //  (reponse: 90 ae 23 00 00 00 00 00)
+		0x18, 0x00, 0x05, 0x83  //  (reponse: 00 00 00 00)	
+	} ;
+	socket.write_some(boost::asio::buffer(startBuf), error);
+
+	for(;;)
+	{
+		boost::array<char, 5000> buf;
+		size_t len = socket.read_some(boost::asio::buffer(buf), error);
+		if (error == boost::asio::error::eof)
+			break; // Connection closed cleanly by peer.
+		else if (error)
+			throw boost::system::system_error(error); // Some other error.
+
+		std::cout << "msg of " << len << " bytes." << std::endl;
+		for(int i=0; i<len; ++i)
+		{
+			std::cout << "0x" << std::hex << std::uppercase << (int)buf[i] << " ";
+		}
+		std::cout << std::endl;
+
+		
+		if (AtEnd())
+			break;
+		// sub 16ms sleep, depends on available hardware for accuracy
+		boost::this_thread::sleep(boost::posix_time::microseconds(GetMicrosecondInterval()));
+	}
+}
+
+void SocketReader::Abort()
+{
+	LogSource::Abort();
+	m_thread.join();
 }
 
 void SocketReader::Poll()
