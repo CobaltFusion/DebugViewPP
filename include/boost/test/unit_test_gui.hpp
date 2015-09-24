35aa718e6690c1374273d70e689054c937bd0e67
janwilmans@gmail.com <janwilmans@gmail.com>
2015-09-24 23:26:23 +0200

    removed boosttestui reference

--- include/boost/test/unit_test_gui.hpp
+++ include/boost/test/unit_test_gui.hpp
@@ -3,8 +3,6 @@
 //  (See accompanying file LICENSE_1_0.txt or copy at 
 //  http://www.boost.org/LICENSE_1_0.txt)
 
-//  See http://boosttestui.wordpress.com/ for the boosttestui home page.
-
 #ifndef BOOST_TEST_UNIT_TEST_GUI_HPP
 #define BOOST_TEST_UNIT_TEST_GUI_HPP
 
