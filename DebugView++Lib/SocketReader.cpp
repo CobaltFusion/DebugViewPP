b2d6526a03affbc768398e6065f582013d2e2709
gjdevos <Gert-Jan.deVos@phenom-world.com>
2015-10-16 17:56:01 +0200

    SocketReader2

--- DebugView++Lib/SocketReader.cpp
+++ DebugView++Lib/SocketReader.cpp
@@ -55,6 +55,77 @@ typedef std::unique_ptr<void, SocketDeleter> Socket;
 
 } // namespace Win32
 
+namespace debugviewpp {
+
+class SocketReader2 : public LogSource
+{
+public:
+	explicit SocketReader2(Timer& timer, SourceType::type sourceType, ILineBuffer& linebuffer, int port) :
+		LogSource(timer, sourceType, linebuffer),
+		m_wsa(2, 2),
+		m_socket( WSASocket(AF_INET, SOCK_DGRAM, IPPROTO_UDP, nullptr, 0, WSA_FLAG_OVERLAPPED) ),
+		m_event( Win32::CreateEvent(nullptr, false, false, nullptr) )
+	{
+		if (m_socket.get() == INVALID_SOCKET)
+			Win32::WSAThrowLastError("WSASocket");
+
+		sockaddr_in sa;
+		sa.sin_family = AF_INET;
+		sa.sin_addr.s_addr = htonl(INADDR_ANY);
+		sa.sin_port = htons(port);
+		if (bind(m_socket.get(), reinterpret_cast<sockaddr*>(&sa), sizeof(sa)) == SOCKET_ERROR)
+			Win32::WSAThrowLastError("bind");
+
+	}
+
+	virtual HANDLE GetHandle() const
+	{
+		return m_event.get();
+	}
+
+	virtual void Notify()
+	{
+		Add(m_buffer.data());
+	}
+
+	bool BeginReceive()
+	{
+		WSABUF buffers[] = { m_buffer.size(), m_buffer.data() };
+
+		DWORD count;
+		DWORD flags;
+		sockaddr_in from;
+		int fromlen;
+		WSAOVERLAPPED overlapped;
+		overlapped.hEvent = m_event.get();
+		if (WSARecvFrom(m_socket.get(), buffers, 1, &count, &flags, reinterpret_cast<sockaddr*>(&from), &fromlen, &overlapped, nullptr) == SOCKET_ERROR)
+		{
+			auto err = WSAGetLastError();
+			if (err != WSA_IO_PENDING)
+				Win32::ThrowWin32Error(err, "WSARecvFrom");
+			return false;
+		}
+		return true;
+	}
+
+	void CompleteReceive()
+	{
+		DWORD count;
+		DWORD flags;
+		WSAOVERLAPPED overlapped;
+		overlapped.hEvent = m_event.get();
+		int rc = WSAGetOverlappedResult(m_socket.get(), &overlapped, &count, FALSE, &flags);
+		if (rc == 0)
+			Win32::WSAThrowLastError("WSAGetOverlappedResult");
+	}
+
+private:
+	Win32::WinsockInitialization m_wsa;
+	Win32::Socket m_socket;
+	Win32::Handle m_event;
+	boost::array<char, 2000> m_buffer;
+};
+
 void OverlappedUdpReaderSample(int port)
 {
 	Win32::WinsockInitialization wsa(2, 2);
@@ -93,8 +164,6 @@ void OverlappedUdpReaderSample(int port)
 	}
 
 }
-
-namespace debugviewpp {
 
 SocketReader::SocketReader(Timer& timer, ILineBuffer& lineBuffer, int port) :
 	PassiveLogSource(timer, SourceType::Pipe, lineBuffer, 40),
