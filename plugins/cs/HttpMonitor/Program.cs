b08bf00b9e39d929bf188242ab2678f48f510ee0
Jan Wilmans <janwilmans@gmail.com>
2016-03-15 01:08:00 +0100

    added httpmonitor plugin

---
+++ plugins/cs/HttpMonitor/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace HttpMonitor
+{
+	class Log
+	{
+		public Log(string url)
+		{
+			this.url = url;
+		}
+
+		public HttpWebRequest CreateWebRequest(string url)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Timeout = Settings.HttpTimeout;
+			request.ProtocolVersion = HttpVersion.Version11;
+			return request;
+		}
+
+
+		public bool IsRangeAvailable()
+		{
+			var request = CreateWebRequest(url);
+			//request.Accept = "image/*";
+			request.AddRange(0,1);
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+			if (Settings.Verbose)
+			{
+				Console.WriteLine("StatusCode: " + response.StatusCode + " (" + (int)response.StatusCode + ")");
+
+				foreach (var headerkey in response.Headers.AllKeys)
+				{
+					Console.WriteLine(headerkey + " : " + response.Headers[headerkey]);
+				}
+			}
+			return (response.StatusCode == HttpStatusCode.PartialContent);
+		}
+
+		public void TraceDiff(List<string> lastLines, List<string> lines)
+		{
+			if (lines.Count > lastLines.Count)
+			{
+				for (int i = lastLines.Count; i < lines.Count; i++)
+				{
+					Trace.Write(lines[i]);
+				}
+			}
+			else
+			{
+				foreach (var line in lines)
+				{
+					Trace.Write(line);
+				}
+			}
+		}
+
+		public void Monitor()
+		{
+			if (IsRangeAvailable())
+			{
+				Util.DebugWrite("HTTP/1.1 section 14.35.1 Byte Ranges available");
+			}
+			else
+			{
+				Util.DebugWrite("HTTP/1.1 section 14.35.1 Byte Ranges NOT available");
+			}
+
+			var lastLines = Get();
+			foreach (var line in Get())
+				Trace.Write(line);
+			while (true)
+			{
+				Thread.Sleep(Settings.PollInterval);
+				var lines = Get();
+				if (lines.Count != lastLines.Count)
+				{
+					TraceDiff(lastLines, lines);
+				}
+				lastLines = lines;
+			}
+		}
+
+		public List<string> Get()
+		{
+			List<string> result = new List<string>();
+			var request = CreateWebRequest(url);
+
+			try
+			{
+				using (WebResponse response = (HttpWebResponse)request.GetResponse())
+				{
+
+					var encoding = ASCIIEncoding.ASCII;
+					using (var reader = new System.IO.StreamReader(response.GetResponseStream(), encoding))
+					{
+						while (!reader.EndOfStream)
+						{
+							result.Add(reader.ReadLine());
+						}
+					}
+				}
+			}
+			catch (WebException e)
+			{
+				if (Settings.Verbose)
+				{
+					Util.DebugWrite(e.ToString());
+				}
+				else
+				{
+					Util.DebugWrite(e.Message);
+				}
+				Trace.WriteLine("--- HttpMonitor of '" + url + "' stopped ---");
+				throw;
+			}
+			return result;
+		}
+
+		private string url;
+	}
+
+	static class Util
+	{
+		static public void DebugWrite(string msg)
+		{
+			Console.WriteLine(msg);
+			Trace.WriteLine(msg);
+		}
+	}
+
+	static class Settings
+	{
+		static public bool Verbose;
+		static public int PollInterval = 5000;
+		static public int HttpTimeout = 5000;
+	}
+
+	class DebugViewPlugin
+	{
+		public static int IsIdentified(string argument)
+		{
+			if (string.IsNullOrEmpty(argument)) return -1;
+			if (argument.ToLower().StartsWith("http")) return 1;
+			return -1;
+		}
+
+		public static void ProcessCommandline(string[] args)
+		{
+			if (args.Length < 1) return;
+
+			string command = args[0].ToLower();
+			List<string> list = new List<string>(args);
+			list.RemoveAt(0);
+			string argument = string.Join(" ", list.ToArray());
+
+			if (command.Contains("--info"))
+			{
+				// multiple 'input type' entries are allowed
+				// input type: .dblog				// for .dblog files
+				// input type: URL					// for any URL 
+				// input type: URL-http				// for urls starting with http
+				Console.WriteLine("input type: http");
+				Console.WriteLine("input type: https");
+				Console.WriteLine("outputdebugstring: yes");
+				System.Environment.Exit(0);
+			}
+			else if (command.Contains("--identify"))
+			{
+				// exit code 1 means we recognize the specified file, otherwise exit code -1;
+				System.Environment.Exit(IsIdentified(argument));
+			}
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DebugViewPlugin.ProcessCommandline(args);
+			foreach (var arg in args)
+			{
+				if (arg.ToLower().Contains("--verbose"))
+					Settings.Verbose = true;
+			}
+
+			if (args.Length < 1)
+			{
+				Console.WriteLine("usage: httpmonitor <url>");
+				return;
+			}
+			string url = args[0];
+		
+			Console.WriteLine("Poll interval: " + Settings.PollInterval);
+			Console.WriteLine("URL: " + url);
+			Console.WriteLine("Monitoring...");
+
+			Log log = new Log(url);
+			log.Monitor();
+		}
+	}
+}
