e1d19f8c6ab0001d65c06007326db4730da8b3ca
Jan Wilmans <janwilmans@gmail.com>
2016-12-18 17:52:31 +0100

    added OutputForwarderVSIX visual studio extension

---
+++ OutputForwarderVSIX/Forwarder.cs
@@ -0,0 +1,133 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="Forwarder.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text.Editor;
+using System.Diagnostics;
+
+namespace OutputForwarderVSIX
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class Forwarder
+    {
+        ///// <summary>
+        ///// Command ID.
+        ///// </summary>
+        //public const int CommandId = 0x0100;
+
+        ///// <summary>
+        ///// Command menu group (command set GUID).
+        ///// </summary>
+        //public static readonly Guid CommandSet = new Guid("d337dc6e-ca3f-4b71-a608-575e3eabe693");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Forwarder"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private Forwarder(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+            InstallForwarder(package);
+        }
+
+        private void InstallForwarder(Package package)
+        {
+            IVsOutputWindow outWindow = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            Guid paneGuid = VSConstants.GUID_OutWindowDebugPane;
+            IVsOutputWindowPane debugPane;
+            outWindow.GetPane(ref paneGuid, out debugPane);
+            debugPane.Activate(); // Brings this pane into view
+
+            IVsUserData userData = (IVsUserData)debugPane;
+            object o;
+            Guid guidViewHost = DefGuidList.guidIWpfTextViewHost;
+            userData.GetData(ref guidViewHost, out o);
+
+            IWpfTextViewHost viewHost = (IWpfTextViewHost)o;
+            m_textView = viewHost.TextView;
+            m_textView.TextBuffer.Changed += new EventHandler<Microsoft.VisualStudio.Text.TextContentChangedEventArgs>(TextBuffer_Changed);
+
+            debugPane.OutputString("Cobalt Fusion Output Window forwarding initialized.\n");
+            Trace.AutoFlush = true;
+        }
+
+        void TextBuffer_Changed(object sender, Microsoft.VisualStudio.Text.TextContentChangedEventArgs e)
+        {
+            foreach (var change in e.Changes)
+            {
+                string text = change.NewText.ToString();
+                while (text.Length > 4090)
+                {
+                    Trace.Write("Following lines exceeded 4090 chars!\n");
+                    var part = text.Substring(0, 4090);
+                    int nlIndex = part.LastIndexOf("\n", 0);
+                    if (nlIndex == -1)
+                    {
+                        Trace.Write(part);
+                        text = text.Substring(4091);
+                    }
+                    else
+                    {
+                        Trace.Write(part.Substring(0, nlIndex + 1));
+                        text = text.Substring(nlIndex + 1);
+                    }
+                }
+                Trace.Write(text);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static Forwarder Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new Forwarder(package);
+        }
+
+        private IWpfTextView m_textView;
+    }
+}
---
+++ OutputForwarderVSIX/ForwarderPackage.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ForwarderPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace OutputForwarderVSIX
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    /// 
+
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    //[ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(ForwarderPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string)]  // this makes sure the extention is initialized at startup
+    public sealed class ForwarderPackage : Package
+    {
+        /// <summary>
+        /// ForwarderPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "de58e538-ebbb-4c53-b925-a721712be49f";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Forwarder"/> class.
+        /// </summary>
+        public ForwarderPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Forwarder.Initialize(this);
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
