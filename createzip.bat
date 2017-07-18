d107201930bb96f500f625fc38f9768fcc04953d
Jan Wilmans <janwilmans@gmail.com>
2017-07-18 19:09:50 +0200

    fix distribution of the win32 version of the visual studio plugin

--- createzip.bat
+++ createzip.bat
@@ -4,11 +4,16 @@ pushd %~dp0
 setlocal
 
 if [%1] == [] (
-    echo Usage: createzip ^<binary path^>
+    echo Usage: createzip ^<binary path^> ^<project path^>
+    goto exit
+)
+if [%2] == [] (
+    echo Usage: createzip ^<binary path^> ^<project path^>
     goto exit
 )
 
 set bin_dir=%1
+set project_dir=%2
 
 :: paths can be absolute or relative to the location of this batch file
 set zip_bin=.\zip.exe
@@ -28,7 +33,9 @@ if not exist %upx_bin% (
 %upx_bin% %bin_dir%\DebugView++.exe
 %upx_bin% %bin_dir%\DebugViewConsole.exe
 
+:: notice we pack the win32 .vsix in any case (so also in x64 builds), and this is correct.
+
 :zip
-%zip_bin% -j %bin_dir%\DebugView++.zip %bin_dir%\DebugView++.exe %bin_dir%\DebugViewConsole.exe %bin_dir%\*.vsix
+%zip_bin% -j %bin_dir%\DebugView++.zip %bin_dir%\DebugView++.exe %bin_dir%\DebugViewConsole.exe %project_dir%\release\*.vsix
 
 :exit
