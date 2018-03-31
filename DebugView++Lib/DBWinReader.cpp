9058c4199597196654d3b2582bec66836d1354fd
Roy Lazy <lazyroy@gmail.com>
2018-04-01 00:02:38 +0300

    Proof of concept

--- DebugView++Lib/DBWinReader.cpp
+++ DebugView++Lib/DBWinReader.cpp
@@ -11,6 +11,8 @@
 #include "DebugView++Lib/ProcessInfo.h"
 #include "DebugView++Lib/LineBuffer.h"
 #include "CobaltFusion/stringbuilder.h"
+#include <AccCtrl.h>
+#include <Aclapi.h>
 
 namespace fusion {
 namespace debugviewpp {
@@ -28,18 +30,24 @@ Win32::Handle CreateDBWinBufferMapping(bool global)
 	Win32::Handle hMap(CreateFileMapping(nullptr, nullptr, PAGE_READWRITE, 0, sizeof(DbWinBuffer), GetDBWinName(global, L"DBWIN_BUFFER").c_str()));
 	if (GetLastError() == ERROR_ALREADY_EXISTS)
 		throw std::runtime_error("CreateDBWinBufferMapping");
-	return hMap;
+	SetSecurityInfo(hMap.get(), SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, nullptr, nullptr, nullptr, nullptr);
+    return hMap;
 }
 
 DBWinReader::DBWinReader(Timer& timer, ILineBuffer& linebuffer, bool global) :
 	LogSource(timer, SourceType::System, linebuffer),
 	m_hBuffer(CreateDBWinBufferMapping(global)),
+
 	m_dbWinBufferReady(Win32::CreateEvent(nullptr, false, true, GetDBWinName(global, L"DBWIN_BUFFER_READY").c_str())),
 	m_dbWinDataReady(Win32::CreateEvent(nullptr, false, false, GetDBWinName(global, L"DBWIN_DATA_READY").c_str())),
 	m_mappedViewOfFile(m_hBuffer.get(), PAGE_READONLY, 0, 0, sizeof(DbWinBuffer)),
 	m_dbWinBuffer(static_cast<const DbWinBuffer*>(m_mappedViewOfFile.Ptr()))
 {
 	SetDescription(global ? L"Global Win32 Messages" : L"Win32 Messages");
+
+	SetSecurityInfo(m_dbWinBufferReady.get(), SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, nullptr, nullptr, nullptr, nullptr);
+	SetSecurityInfo(m_dbWinDataReady.get(), SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, nullptr, nullptr, nullptr, nullptr);
+
 	Win32::SetEvent(m_dbWinBufferReady);
 }
 
