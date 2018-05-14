d99c3ca05cd0bdb2a8a40c876e237c0a1bfadb06
Jan Wilmans <janwilmans@gmail.com>
2018-05-14 08:51:36 +0200

    added buildtiming.bat

---
+++ buildtiming.bat
@@ -0,0 +1,4 @@
+:: Time Elapsed 00:01:22.63
+:: Time Elapsed 00:01:24.46
+
+"C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin\MSBuild.exe" "DebugView++.sln" /t:rebuild /p:configuation=release
