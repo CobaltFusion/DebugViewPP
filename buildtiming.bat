dd11ded94582cfecafb6fc0066e875f35467d64f
Jan Wilmans <janwilmans@gmail.com>
2018-05-14 09:00:53 +0200

    added /MP (multi-processor compilation), effect: -33% build time 84 sec -> 57 sec.

--- buildtiming.bat
+++ buildtiming.bat
@@ -1,4 +1,5 @@
 :: Time Elapsed 00:01:22.63
 :: Time Elapsed 00:01:24.46
+:: +/MP Time Elapsed 00:00:57.29 (-33%)
 
 "C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin\MSBuild.exe" "DebugView++.sln" /t:rebuild /p:configuation=release
